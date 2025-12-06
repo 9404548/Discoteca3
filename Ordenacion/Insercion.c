f1da2ca30948a46e7cdea2e41a6b1aa736ae9f48
Rodrigo-dFL <9406800@alumnos.ufv.es>
2025-12-06 15:41:40 +0100

    inserción

--- Ordenacion/Insercion.c
+++ Ordenacion/Insercion.c
@@ -5,27 +5,74 @@
  * Descripción: Ordena el array de fichas de libros en un doble puntero a las fichas de Libros,
  *              por lo que el movimient de datos se reduce al cambio de punteros.
  *              Se usa el algoritmo de ordenación de Inserción.
- * Reglas de uso: 
+ * Reglas de uso:
  * Código de Retorno: LIBRO **: Doble puntero ordenado por el criterio pedido a las fichas de libros.
  * Programador: JMSM (Sept-24)
  *****************************************/
 
 #include "../Discoteca.h"
 
-DISCO **Insercion(DISCO *Ficha,int Campo)
+DISCO **Insercion(DISCO *Ficha, int Campo)
 {
     DISCO **Orden;
     struct timeval inicio, fin;
-    
-    //Añade aquí la definición del resto de variables necesarias
+    int i, j;
+    DISCO *aux;
+    char Autor1[100], Autor2[100];
 
-    gettimeofday(&inicio,NULL);
-    Orden=InitOrden(Ficha);
+    gettimeofday(&inicio, NULL);
+    Orden = InitOrden(Ficha);
 
-    //Código del Alumno del Algoritmo de ordenación por Inserción
-    
-    gettimeofday(&fin,NULL);
-    Estadisticas.TiempoInsercion=DifTiempo(inicio,fin);
+    for (i = 1; i < Estadisticas.NumeroFichas; i++)
+    {
+        // Guardamos la ficha en una variable auxiliar para comparar
+        aux = Orden[i];
+        j = i - 1;
+        // Mientras j sea válido y Orden[j] sea mayor que aux, desplazamos los elementos
+        while (j >= 0)
+        {
+            int comparador;
+            if (Campo == ORDEN_POR_AUTOR)
+            {
+                // Ponemos los autores como "Apellido, Nombre" para compararlos
+                strcpy(Autor1, Orden[j]->ApellAutor);
+                if (Orden[j]->NomAutor != NULL)
+                {
+                    strcat(Autor1, ", ");
+                    strcat(Autor1, Orden[j]->NomAutor);
+                }
+                strcpy(Autor2, aux->ApellAutor);
+                if (aux->NomAutor != NULL)
+                {
+                    strcat(Autor2, ", ");
+                    strcat(Autor2, aux->NomAutor);
+                }
+                comparador = strcmp(Autor1, Autor2);
+            }
+            else
+            {
+                // Si el campo es "por titulo", se compara directamente
+                comparador = strcmp(Orden[j]->Obra, aux->Obra);
+            }
 
-    return(Orden);
+            /***********************************************************************
+             * Para ordenar:                                                       *
+             * Si Autor1 es igual que Autor2 -> comparador = 0                     *
+             * Si Autor1 es lexicográficamente menor que Autor2 -> comparador < 0  *
+             * Si Autor1 es lexicográficamente mayor que Autor2 -> comparador > 0  *
+             **********************************************************************/
+
+            if (comparador <= 0)
+                break;
+            Orden[j + 1] = Orden[j];
+            j--;
+        }
+        Orden[j + 1] = aux;
+    }
+
+    // Calcular el tiempo de finalización
+    gettimeofday(&fin, NULL);
+    Estadisticas.TiempoInsercion = DifTiempo(inicio, fin);
+
+    return (Orden);
 }
