6391bb1f7cc572a70cba044544f27c718825fbde
Rodrigo-dFL <9406800@alumnos.ufv.es>
2025-12-06 13:29:59 +0100

    selección

--- Ordenacion/Seleccion.c
+++ Ordenacion/Seleccion.c
@@ -5,9 +5,9 @@
  * Descripción: Ordena el array de fichas de libros en un doble puntero a las fichas de Libros,
  *              por lo que el movimient de datos se reduce al cambio de punteros.
  *              Se usa el algoritmo de ordenación de Selección.
- * Reglas de uso: 
+ * Reglas de uso:
  * Código de Retorno: LIBRO **: Doble puntero ordenado por el criterio pedido a las fichas de libros.
- * Programador: JMSM (Sept-24)
+ * Programador: Rodrigo-dFL (Dec 25)
  *****************************************/
 
 #include "../Discoteca.h"
@@ -16,16 +16,70 @@ DISCO **Seleccion(DISCO *Ficha, int Campo)
 {
     DISCO **Orden;
     struct timeval inicio, fin;
-    
-    //Añade aquí la definición del resto de variables necesarias
 
-    gettimeofday(&inicio,NULL);
-    Orden=InitOrden(Ficha);
+    // Añade aquí la definición del resto de variables necesarias
+    int i, j, min_indx;
+    DISCO *tmp;
+    char Autor1[100], Autor2[100];
 
-    //Código del Alumno del Algoritmo de ordenación por Selección
+    gettimeofday(&inicio, NULL);
+    Orden = InitOrden(Ficha);
 
-    gettimeofday(&fin,NULL);
-    Estadisticas.TiempoSeleccion=DifTiempo(inicio,fin);
+    // Código del Alumno del Algoritmo de ordenación por Selección
+    for (i = 0; i < Estadisticas.NumeroFichas - 1; i++)
+    {
+        min_indx = i;
+        // Encontrar el elemento mínimo en la parte no ordenada
+        for (j = i + 1; j < Estadisticas.NumeroFichas; j++)
+        {
+            int comparador = 0;
+            // Si se ordena por autor, ponemos el campo como "Apellido, Nombre"
+            if (Campo == ORDEN_POR_AUTOR)
+            {
+                strcpy(Autor1, Orden[j]->ApellAutor);
+                if (Orden[j]->NomAutor != NULL)
+                {
+                    strcat(Autor1, ", ");
+                    strcat(Autor1, Orden[j]->NomAutor);
+                }
+                strcpy(Autor2, Orden[min_indx]->ApellAutor);
+                if (Orden[min_indx]->NomAutor != NULL)
+                {
+                    strcat(Autor2, ", ");
+                    strcat(Autor2, Orden[min_indx]->NomAutor);
+                }
+                // Comparamos los nombres de los autores
+                comparador = strcmp(Autor1, Autor2);
+                /***********************************************************************
+                 * Si Autor1 es igual que Autor2 -> comparador = 0                     *
+                 * Si Autor1 es lexicográficamente menor que Autor2 -> comparador < 0  *
+                 * Si Autor1 es lexicográficamente mayor que Autor2 -> comparador > 0  *
+                 **********************************************************************/
+            }
+            else
+            {
+                // Si se ordena por título, no hace falta hacer cambios
+                comparador = strcmp(Orden[j]->Obra, Orden[min_indx]->Obra);
+            }
 
-    return(Orden);
+            // Si Orden[j] es menor que Orden[min_indx], actualizar min_indx a j
+            if (comparador < 0)
+            {
+                min_indx = j;
+            }
+        }
+
+        // Si se encontró un nuevo mínimo (i es distinto de min_indx), actualizamos Orden[i]
+        if (min_indx != i)
+        {
+            tmp = Orden[i];
+            Orden[i] = Orden[min_indx];
+            Orden[min_indx] = tmp;
+        }
+    }
+
+    gettimeofday(&fin, NULL);
+    Estadisticas.TiempoSeleccion = DifTiempo(inicio, fin);
+
+    return (Orden);
 }
