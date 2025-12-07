325be846e5f238951c2f9ea9b41ecf32fb6741db
manuelcpedrero <manuelcpedrero@gmail.com>
2025-12-07 16:39:35 +0100

    Seleccion completo

--- Ordenacion/Seleccion.c
+++ Ordenacion/Seleccion.c
@@ -1,50 +1,30 @@
 /*****************************************
  * Nombre: Seleccion
- * Argumentos: LIBRO *Ficha:       Array de las fichas de libros
+ * Argumentos: DISCO *Ficha:       Array de las fichas de discos
  *             int campo:          Campo por el que hacer la ordenación (Título o Autor)
- * Descripción: Ordena el array de fichas de libros en un doble puntero a las fichas de Libros,
- *              por lo que el movimient de datos se reduce al cambio de punteros.
+ * Descripción: Ordena el array de fichas de discos en un doble puntero a las fichas de Discos,
+ *              por lo que el movimiento de datos se reduce al cambio de punteros.
  *              Se usa el algoritmo de ordenación de Selección.
  * Reglas de uso: 
- * Código de Retorno: LIBRO **: Doble puntero ordenado por el criterio pedido a las fichas de libros.
- * Programador: JMSM (Sept-24)
+ * Código de Retorno: DISCO **: Doble puntero ordenado por el criterio pedido a las fichas de discos.
+ * Programador: JMSM (Sept-25)
  *****************************************/
 
 #include "../Discoteca.h"
-
-// Función auxiliar para comparar dos discos según el campo especificado
-// static int comparar(DISCO *a, DISCO *b, int Campo)
-// {
-//     if (Campo == ORDEN_POR_AUTOR) {
-//         char Autor1[256], Autor2[256];
-//         strcpy(Autor1, a->ApellAutor);
-//         if (a->NomAutor != NULL) {
-//             strcat(Autor1, ", ");
-//             strcat(Autor1, a->NomAutor);
-//         }
-//         strcpy(Autor2, b->ApellAutor);
-//         if (b->NomAutor != NULL) {
-//             strcat(Autor2, ", ");
-//             strcat(Autor2, b->NomAutor);
-//         }
-//         return strcmp(Autor1, Autor2);
-//     } else {
-//         return strcmp(a->Obra, b->Obra);
-//     }
-// }
 
 DISCO **Seleccion(DISCO *Ficha, int Campo)
 {
     DISCO **Orden;
     struct timeval inicio, fin;
-    int i, j, min, comp;
+    int i, j, min;
+    char Autor1[100], Autor2[100];
     int n = Estadisticas.NumeroFichas;
     DISCO *pAux;
     // Añade aquí la definición del resto de variables necesarias
 
     gettimeofday(&inicio, NULL);
     Orden = InitOrden(Ficha);
-    pAux = Orden[0];
+
     // Bucle que avanza de casilla en casilla
     for (i = 0; i < n - 1; i++)
     {
@@ -64,22 +44,21 @@ DISCO **Seleccion(DISCO *Ficha, int Campo)
             }
             else if (Campo == ORDEN_POR_AUTOR)
             {
-                // Comparamos los apellidos
-                comp = strcmp(Orden[j]->ApellAutor, Orden[min]->ApellAutor);
-                if (comp < 0)
-                {
-                    // El menor de los apellidos
-                    min = j;
+                // Concateno el apellido y el nombre de autores
+                strcpy(Autor1, Orden[j]->ApellAutor);
+                if(Orden[j]->NomAutor != NULL){
+                    strcat(Autor1, ", ");
+                    strcat(Autor1, Orden[j]->NomAutor);
                 }
-                // Si el apellido es igual
-                else if (comp == 0)
-                {
-                    // Comparamos con el nombre
-                    if (strcmp(Orden[j]->NomAutor, Orden[min]->NomAutor) < 0)
-                    {
-                        // El menor de los nombres
-                        min = j;
-                    }
+                // Concateno el apellido y el nombre del autor que es el menor
+                strcpy(Autor2, Orden[min]->ApellAutor);
+                if(Orden[min]->NomAutor != NULL){
+                    strcat(Autor2, ", ");
+                    strcat(Autor2, Orden[min]->NomAutor);
+                }
+                // Se compara y si es menor, se actualiza min
+                if(strcmp(Autor1, Autor2) < 0){
+                    min = j;
                 }
             }
         }
